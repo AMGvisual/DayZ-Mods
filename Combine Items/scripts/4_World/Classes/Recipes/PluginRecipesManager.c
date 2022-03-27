5d08353afc83bace0be05a70b1f648a78c8798cc
AMG <52355949+amgvisual@users.noreply.github.com>
2022-03-27 03:18:39 -0400

    Create PluginRecipesManager.c

---
+++ Combine Items/scripts/4_World/Classes/Recipes/PluginRecipesManager.c
@@ -0,0 +1,26 @@
+modded class PluginRecipesManager 
+{
+    override void RegisterRecipies() 
+	{   
+		super.RegisterRecipies();
+
+		//Combining
+		RegisterRecipe(new CombineRice);
+		RegisterRecipe(new CombineLime);
+		RegisterRecipe(new CombineSewingKit);
+		RegisterRecipe(new CombineLeatherSewingKit);
+		RegisterRecipe(new CombineCleaningKit);
+		RegisterRecipe(new CombineEpoxy);
+		RegisterRecipe(new CombineDuctTape);
+		RegisterRecipe(new CombineAntibiotics);
+		RegisterRecipe(new CombineDisinfectantSpray);
+		RegisterRecipe(new CombineDisinfectantAlcohol);
+		RegisterRecipe(new CombineBandages);
+		RegisterRecipe(new CombineCBDGummies);
+		RegisterRecipe(new CombineIndicaGummies);
+		RegisterRecipe(new CombineSativaGummies);
+
+    }
+};
+
+
