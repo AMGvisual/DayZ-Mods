bef908e2b5e7b28f39a209315ebb07f97587ff84
AMGvisual <info@amgvisual.com>
2023-03-16 00:16:19 -0400

    Wooden crate for admins to store lots of things

--- Admin Crate/config.cpp
+++ Admin Crate/config.cpp
@@ -16,14 +16,11 @@ class CfgVehicles
 	class WoodenCrate;
 	class WoodenCrate_Admin: WoodenCrate
 	{
-		scope=2;
 		displayName="ADMIN CRATE";
 		descriptionShort="Wooden crate for admins to store lots of things";
 		class Cargo
 		{
 			itemsCargoSize[]={10,500};
-			openable=0;
-			allowOwnedCargoManipulation=1;
 		};
 	};
 };
