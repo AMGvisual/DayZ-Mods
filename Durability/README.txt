a7ce2e5f05222db5d83d81c7482b462bd19584ac
AMG <info@amgvisual.com>
2023-05-19 18:19:23 -0400

    A simple mod that increases the durability of various items such as knives, melee weapons and crossbow bolts  *DISCLAIMER* - THIS MOD IS FREE AND OPEN SOURCE. DO WITH IT AS YOU PLEASE

--- Durability/README.txt
+++ Durability/README.txt
@@ -1,3 +1,3 @@
-A simple mod that increases the durability of various items such as knives and melee weapons
+A simple mod that increases the durability of various items such as knives, melee weapons and crossbow bolts
 
 *DISCLAIMER* - THIS MOD IS FREE AND OPEN SOURCE. DO WITH IT AS YOU PLEASE
--- Durability/config.cpp
+++ Durability/config.cpp
@@ -8,15 +8,14 @@ class CfgPatches
 		requiredAddons[]=
 		{
 			"DZ_Data",
-			"DZ_Weapons_Melee"
+			"DZ_Weapons_Melee",
+			"DZ_Weapons_Magazines"
 		};
 	};
 };
 class CfgVehicles
 {
 	class Inventory_Base;
-	class Ammunition_Base;
-	class Bolt_Base;
 	class StoneKnife: Inventory_Base
 	{
 		class DamageSystem
@@ -104,6 +103,49 @@ class CfgVehicles
 				class Health
 				{
 					hitpoints=300;  // Default: 150
+				};
+			};
+		};
+	};
+};
+class CfgMagazines
+{
+	class Ammunition_Base;
+	class Ammo_HuntingBolt: Ammunition_Base
+	{
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=360;  // Default: 120
+				};
+			};
+		};
+	};
+	class Ammo_ImprovisedBolt_1: Ammunition_Base
+	{
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=195;  // Default: 65
+				};
+			};
+		};
+	};
+	class Ammo_ImprovisedBolt_2: Ammunition_Base
+	{
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=255;  // Default: 85
 				};
 			};
 		};
